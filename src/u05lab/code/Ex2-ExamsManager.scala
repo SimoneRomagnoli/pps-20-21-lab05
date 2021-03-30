9b86402197dafdc5c54a8e2226650725aebd2289
SimoneRomagnoli <simone.romagnoli10@studio.unibo.it>
2021-03-30 14:15:31 +0200

    implementation of exams manager; need to test

--- src/u05lab/code/Ex2-ExamsManager.scala
+++ src/u05lab/code/Ex2-ExamsManager.scala
@@ -1,5 +1,7 @@
 package u05lab.code
 
+import scala.jdk.CollectionConverters._
+
 //-----------EXAM RESULTS-----------
 sealed trait Kind
 
@@ -15,9 +17,7 @@ object Kind {
 
 sealed trait Result {
   def getKind:Kind
-
   def getEvaluation: Option[Int]
-
   def cumLaude: Boolean
 }
 
@@ -25,11 +25,8 @@ sealed trait Result {
 //-----------EXAM RESULTS FACTORY-----------
 sealed trait ExamResultFactory {
   def failed:Result
-
   def retired:Result
-
   def succeded(evaluation:Int):Result
-
   def succededCumLaude:Result
 }
 
@@ -39,7 +36,8 @@ object ExamResult extends ExamResultFactory {
   val PERFECT = 30
 
   private case class AbstractExamResult(kind:Kind, eval:Option[Int] = Option.empty, laude:Boolean = false) extends Result {
-    override def getKind: Kind = kind
+    override def getKind: Kind =
+      kind
 
     override def getEvaluation: Option[Int] = kind match {
       case SUCCEDED() =>
@@ -48,50 +46,68 @@ object ExamResult extends ExamResultFactory {
       case _ => Option.empty
     }
 
-    override def cumLaude: Boolean = eval.nonEmpty && laude
+    override def cumLaude: Boolean =
+      eval.nonEmpty && laude
   }
 
-  override def failed: Result = AbstractExamResult(FAILED())
+  override def failed: Result =
+    AbstractExamResult(FAILED())
 
-  override def retired: Result = AbstractExamResult(RETIRED())
+  override def retired: Result =
+    AbstractExamResult(RETIRED())
 
-  override def succeded(evaluation: Int): Result = AbstractExamResult(SUCCEDED(), Option.apply(evaluation))
+  override def succeded(evaluation: Int): Result =
+    AbstractExamResult(SUCCEDED(), Option.apply(evaluation))
 
-  override def succededCumLaude: Result = AbstractExamResult(SUCCEDED(), Option.apply(PERFECT), true)
+  override def succededCumLaude: Result =
+    AbstractExamResult(SUCCEDED(), Option.apply(PERFECT), true)
 }
 
 
 //-----------EXAM MANAGER-----------
 sealed trait ExamsManager {
   def createNewCall(call:String): Unit
-
-  def addStudentResult(call:String, student:String, result:Kind)
-
+  def addStudentResult(call:String, student:String, result:Result):Unit
   def getAllStudentsFromCall(call:String):Set[String]
-
   def getEvaluationsMapFromCall(call:String):Map[String, Int]
-
   def getResultsMapFromStudent(student:String):Map[String, String]
-
   def getBestResultFromStudent(student:String):Option[Int]
 
 }
 
 case class ExamsManagerImpl() extends ExamsManager {
-  val exams: Map[String, Map[String, Result]] = Map.empty
   val check: Boolean => Unit = c => if(!c) throw new IllegalArgumentException
 
-  override def createNewCall(call: String): Unit = ???
+  var exams: Map[String, Map[String, Result]] = Map.empty
 
-  override def addStudentResult(call: String, student: String, result: Kind): Unit = ???
+  override def createNewCall(call: String): Unit = {
+    check(!exams.contains(call))
+    exams += call -> Map.empty
+  }
 
-  override def getAllStudentsFromCall(call: String): Set[String] = ???
+  override def addStudentResult(call: String, student: String, result: Result): Unit = {
+    check(exams.contains(call))
+    check(!exams(call).contains(student))
+    exams(call) += (student -> result)
+  }
 
-  override def getEvaluationsMapFromCall(call: String): Map[String, Int] = ???
+  override def getAllStudentsFromCall(call: String): Set[String] = {
+    check(exams.contains(call))
+    exams(call).keySet
+  }
 
-  override def getResultsMapFromStudent(student: String): Map[String, String] = ???
+  override def getEvaluationsMapFromCall(call: String): Map[String, Int] = {
+    check(exams.contains(call))
+    exams(call).collect({ case (s, r) if r.getKind == SUCCEDED => s -> r.getEvaluation.get })
+  }
 
-  override def getBestResultFromStudent(student: String): Option[Int] = ???
+  override def getResultsMapFromStudent(student: String): Map[String, String] = {
+    exams.collect({case (exam, evals) if evals.contains(student) => exam -> exams(exam)(student).toString })
+  }
+
+  override def getBestResultFromStudent(student: String): Option[Int] = {
+    exams.collectFirst({case (_, evals) if evals.contains(student) => evals.values.map(r => r.getEvaluation.get).max((x,y) => x-y) })
+  }
 }
 
 
